e6c967474658cdf63ed0411bb3b47dfee6c9f8cd
mizuki <1701129@st.asojuku.ac.jp>
2019-10-29 14:59:58 +0900

    fix : PL変更画面追加

--- cats/src/main/java/cats/controller/ProfileController.java
+++ cats/src/main/java/cats/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package cats.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import cats.dto.HobbyDto;
 import cats.dto.ProfileDto;
+import cats.dto.SchoolDto;
 import cats.service.HobbyService;
 import cats.service.SchoolService;
 import cats.service.StudentService;
@@ -33,14 +37,33 @@ public class ProfileController {
 	 * @param mav
 	 * @return
 	 */
-	@RequestMapping(value = { "/UpdateStart" }, method = RequestMethod.GET)
-	public ModelAndView ProfileUpdate(ModelAndView mav) {
+	@RequestMapping(value = { "/View" }, method = RequestMethod.GET)
+	public ModelAndView ProfileView(ModelAndView mav) {
 
 		ProfileDto dto = new ProfileDto();
 
 
 		mav.addObject("ProfileDto", dto);
+		mav.setViewName("ProfileView");
+		return mav;
+	}
+
+	@RequestMapping(value = { "/Update" }, method = RequestMethod.POST)
+	public ModelAndView ProfileUpdate(ModelAndView mav) {
+
+		//趣味一覧を取得
+		List<HobbyDto> hobbylist = hobbyService.getAllList();
+		//学校一覧を取得
+		List<SchoolDto> schoollist = schoolService.getAllList();
+
+		ProfileDto dto = new ProfileDto();
+
+		mav.addObject("ProfileDto", dto);
 		mav.setViewName("ProfileUpdate");
+		mav.addObject("hobbylist", hobbylist);
+		mav.addObject("schoollist", schoollist);
+
+
 		return mav;
 	}
 
@@ -56,8 +79,15 @@ public class ProfileController {
 		return mav;
 	}
 
-	@RequestMapping(value = { "/Delete" }, method = RequestMethod.GET)
+	/**退会処理
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(value = { "/Delete" }, method = RequestMethod.DELETE)
 	public ModelAndView Profile(ModelAndView mav) {
+
+
+		mav.setViewName("login");
 
 		return mav;
 	}
