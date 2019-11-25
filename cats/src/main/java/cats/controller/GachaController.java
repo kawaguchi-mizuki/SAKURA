84b10987d681bf80072395c526e63932a194f658
mizuki <1701129@st.asojuku.ac.jp>
2019-11-25 15:44:20 +0900

    fix : ガチャ機能

--- cats/src/main/java/cats/controller/GachaController.java
+++ cats/src/main/java/cats/controller/GachaController.java
@@ -2,13 +2,11 @@ package cats.controller;
 
 import java.util.List;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,43 +19,42 @@ import cats.dto.HobbyDto;
 import cats.dto.LoginInfoDto;
 import cats.dto.RequestDto;
 import cats.param.SessionConst;
-
-import cats.service.HobbyService;
 import cats.service.GachaService;
+import cats.service.HobbyService;
 
 //	ガチャ
 @RestController
 @RequestMapping(value = { "/Gacha" })
 public class GachaController {
-	
+
 	@Autowired
 	GachaService gachaService;
-	
+
 	@Autowired
 	HobbyService hobbyService;
-	
+
 	@Autowired
 	HttpSession session;
-	
+
 	@RequestMapping(value = {"/Top"}, method = RequestMethod.GET)
 	public ModelAndView Matching(ModelAndView mav)throws Exception{
-		
+
 		//	ユーザー情報をセッションから取得
 		LoginInfoDto loginInfo = (LoginInfoDto)session.getAttribute(SessionConst.LOGININFO);
-		
+
 		//	趣味一覧を取得
 		List<HobbyDto> hobbylist = hobbyService.getAllList();
-		
+
 		//	ポイント反映
 		int point = loginInfo.getPoint();
-		
+
 		mav.addObject("point",point);
 		mav.addObject("hobbylist", hobbylist);
 		mav.setViewName("Gacha");
 
 		return mav;
 	}
-	
+
 	@RequestMapping(value = {"/Matching"}, method = RequestMethod.GET)
 	public ModelAndView Gacha(
 			RedirectAttributes redirectAttributes,
@@ -65,15 +62,17 @@ public class GachaController {
 			ModelAndView mav,
 			HttpServletRequest request,
 			HttpServletResponse response)throws Exception{
-		
+
 			String ErrMsg ="";
 			String mode = null;
 
 			//	ユーザー情報をセッションから取得
 			LoginInfoDto loginInfo = (LoginInfoDto)session.getAttribute(SessionConst.LOGININFO);
 			System.out.println(loginInfo.getPoint());
-			
-			if(loginInfo.getPoint() > 100) {						
+
+			if(hobbyId == 0){
+				ErrMsg = "趣味を選択してください！";
+			}else if(loginInfo.getPoint() > 100) {
 				GachaDto dto = gachaService.getGacha(loginInfo.getStudentId(),hobbyId);
 				RequestDto set = gachaService.getGachaList(dto.getStudentId(),dto.getHobbyIdSearch(),dto.getStudentSex());
 				loginInfo = gachaService.gachaPoint(loginInfo.getPoint());
@@ -82,15 +81,13 @@ public class GachaController {
 					//	えら
 					gachaService.gachaInsert(set);
 					mode = "update";
-					
+
 				}else {
 					ErrMsg = "マッチング失敗！";
 					loginInfo = gachaService.gachaMissPoint(loginInfo.getPoint());
 				}
-			  }else if(hobbyId == 0){
-				ErrMsg = "趣味を選択してください！";
 			  }else {
-				ErrMsg = "ポイントが足りません！"; 
+				ErrMsg = "ポイントが足りません！";
 			  }
 		//	リダイレクト時に取得したい
 		List<HobbyDto> hobbylist = hobbyService.getAllList();
@@ -101,7 +98,7 @@ public class GachaController {
 		mav.addObject("msg", ErrMsg);
 		mav.addObject("mode", mode);
 		mav.setViewName("Gacha");
-		
+
 		return mav;
 	}
 
