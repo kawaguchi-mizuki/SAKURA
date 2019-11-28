26948ca317a57f6040c87753b032b0a324854aec
mizuki <1701129@st.asojuku.ac.jp>
2019-11-28 14:26:24 +0900

    fix : 調整

--- cats/src/main/java/cats/repository/MessageRepository.java
+++ cats/src/main/java/cats/repository/MessageRepository.java
@@ -2,10 +2,18 @@ package cats.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import cats.entity.MessageTblEntity;
 
 public interface MessageRepository extends JpaRepository<MessageTblEntity,Integer>,JpaSpecificationExecutor<MessageTblEntity> {
+
+	@Query("select m from MessageTblEntity m where talkId = :talkId")
+	String getLastMessage(Integer talkId);
+
+
+
+
 
 
 }
