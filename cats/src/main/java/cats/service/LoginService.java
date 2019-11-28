176096df4c5f185ca5cc23a27384f627d7d9948c
kuriyama <1701132@st.asojuku.ac.jp>
2019-11-28 12:31:27 +0900

    fix : login

--- cats/src/main/java/cats/service/LoginService.java
+++ cats/src/main/java/cats/service/LoginService.java
@@ -144,17 +144,49 @@ public class LoginService {
 	}
 	
 	/**
+	 * ログインボーナスのポイントを取得
+	 * 
+	 * 
+	 */
+	public int HomePoint(Integer num){
+		int point = 0;
+		//	ログインボーナス（ポイント）
+		int[] bar  = {50,100,150,200,300};
+		
+		//	ログインボーナス表示
+		for(int i = 0;i<6;i++) {
+			for(int j = 0;j<bar.length;j++) {
+				if(num == i) {
+					if(i-1 == j) {
+						point = bar[j];	
+					}
+				}
+			}
+		}
+		return point;
+	}
+	
+	/**
 	 * ログアウト処理
 	 * 
 	 * @param token
 	 */
-	public void logout(Integer studentId) {
-
+	public void logout(Integer studentId)
+	{
 		StudentTblEntity StudentTblEntity = loginRepository.getlogout(studentId);
 		if( StudentTblEntity != null ) {
 			loginRepository.delete(StudentTblEntity);
 		}
 	}
+	/**
+	 * エラーチェック処理
+	 * 
+	 * @param token
+	 */
+	//public String LoginCheack() {
+		
+		
+	//}
 	
 	
 }
