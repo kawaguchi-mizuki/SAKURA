b310b8cef10ef2f6fc99ffa71341ccab1e29bb1b
mizuki <1701129@st.asojuku.ac.jp>
2019-12-06 15:14:30 +0900

    fix : setting

--- cats/src/main/java/cats/CatsApplication.java
+++ cats/src/main/java/cats/CatsApplication.java
@@ -2,12 +2,18 @@ package cats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatsApplication {
+public class CatsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatsApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(CatsApplication.class);
 	}
 
 }
